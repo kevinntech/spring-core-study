acfd85379c3a6e7a9906afe0b4d1914af1ac2f5e
kevin <kevinntech@nate.com>
2020-07-20 22:16:56 +0900

    스프링 프레임워크 핵심 기술 - ApplicationContext와 다양한 빈 설정 방법 까지

---
+++ src/main/java/me/whiteship/springapplicationcontext/ApplicationConfig.java
@@ -0,0 +1,11 @@
+package me.whiteship.springapplicationcontext;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+//@ComponentScan(basePackageClasses = DemoApplication.class)
+public class ApplicationConfig {
+
+}
