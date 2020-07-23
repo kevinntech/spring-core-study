e2f5cc9924d596201c644bbffc6626e8ef93f7af
kevin <kevinntech@nate.com>
2020-07-23 10:24:43 +0900

    스프링 프레임워크 핵심 기술 - MessageSource

--- demospring51/src/main/java/me/whiteship/demospring51/AppRunner.java
+++ demospring51/src/main/java/me/whiteship/demospring51/AppRunner.java
@@ -4,25 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.awt.print.Book;
 import java.util.Arrays;
+import java.util.Locale;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ApplicationContext ctx;
-
-    @Autowired
-    BookRepository bookRepository;
+    MessageSource messageSource;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Environment environment = ctx.getEnvironment();
-        System.out.println(environment.getProperty("app.name"));
-        System.out.println(environment.getProperty("app.about"));
+        while(true){
+            Locale.setDefault(new Locale("en","US"));
+            System.out.println(messageSource.getMessage("greeting", new String[]{"keesun"}, Locale.KOREA));
+            System.out.println(messageSource.getMessage("greeting", new String[]{"keesun"}, Locale.getDefault()));
+            Thread.sleep(1000l);
+        }
+
     }
 }
