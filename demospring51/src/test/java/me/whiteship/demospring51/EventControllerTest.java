67694cbfb74ed9fea5c4db9c09b4be8cf4f6c9db
kevin <kevinntech@nate.com>
2020-07-24 18:04:51 +0900

    스프링 프레임워크 핵심 기술 - 데이터 바인딩 추상화: PropertyEditor

---
+++ demospring51/src/test/java/me/whiteship/demospring51/EventControllerTest.java
@@ -0,0 +1,29 @@
+package me.whiteship.demospring51;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class EventControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void getTest() throws Exception {
+        mockMvc.perform(get("/event/1")) // "/event/1"로 get 요청을 하면
+                .andExpect(status().isOk())         // HTTP 응답 200 OK인 것을 바라며
+                .andExpect(content().string("1")); // content가 String으로 1이 나오길 바랍니다.
+
+    }
+}
