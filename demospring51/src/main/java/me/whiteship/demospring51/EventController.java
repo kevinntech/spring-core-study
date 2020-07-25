d0c1cff32fcee2da03203e934c15c684e322eee2
kevin <kevinntech@nate.com>
2020-07-25 10:17:32 +0900

    Converter, Formatter

--- demospring51/src/main/java/me/whiteship/demospring51/EventController.java
+++ demospring51/src/main/java/me/whiteship/demospring51/EventController.java
@@ -9,12 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EventController {
 
-    @InitBinder
-    public void init(WebDataBinder webDataBinder){
-        // webDataBinder에 Event 클래스 타입을 처리할 PropertyEditor를 등록 합니다.
-        webDataBinder.registerCustomEditor(Event.class, new EventEditor());
-    }
-
     @GetMapping("/event/{event}")
     public String getEvent(@PathVariable Event event){
         System.out.println(event);
---
+++ demospring51/src/main/java/me/whiteship/demospring51/WebConfig.java
@@ -0,0 +1,13 @@
+package me.whiteship.demospring51;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addFormatter(new EventFormatter());
+    }
+}
