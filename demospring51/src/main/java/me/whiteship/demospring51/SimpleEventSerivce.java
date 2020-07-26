5bcc9b13577ed4f666da9c7c223133c76e8f90a1
kevin <kevinntech@nate.com>
2020-07-26 13:28:57 +0900

    스프링 프레임워크 핵심 기술 - @AOP

--- demospring51/src/main/java/me/whiteship/demospring51/SimpleEventSerivce.java
+++ demospring51/src/main/java/me/whiteship/demospring51/SimpleEventSerivce.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class SimpleEventSerivce implements EventService {
 
+    @PerLogging
     @Override
     public void createEvent() {
         try {
@@ -16,6 +17,7 @@ public class SimpleEventSerivce implements EventService {
         System.out.println("Created an event");
     }
 
+    @PerLogging
     @Override
     public void publishEvent() {
         try {
